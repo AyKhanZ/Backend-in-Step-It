abce27569acc6e7c6426eff5bb6f6e12bbf776bb
Aykhan <110285172+AyKhanZ@users.noreply.github.com>
2023-07-07 22:57:18 +0400

    System prog first

---
+++ System programming/SystemApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ProcessDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            // - - - - - - - - - - - - - - - - Task 1 - - - - - - - - - - - - - - - -
+            Process process = new Process();
+
+            // Установка имени исполняемого файла для запуска
+            process.StartInfo.FileName = "notepad.exe"; 
+
+            // Запуск процесса
+            process.Start();
+
+            // Ожидание завершения процесса
+            process.WaitForExit();
+
+            // Вывод кода завершения в консоль
+            Console.WriteLine($"Kod end {process.ExitCode}");
+            // - - - - - - - - - - - - - - - - Task 2 - - - - - - - - - - - - - - - -
+            process.StartInfo.FileName = "notepad.exe";
+
+            process.Start();
+
+            Console.WriteLine("Wait the finish of the process - \'1\'\nFinish the process - \'2\'");
+            int choice = Int32.Parse(Console.ReadLine());
+
+            if (choice == 1) process.WaitForExit();
+            else process.Kill();
+
+            Console.WriteLine($"Kod end {process.ExitCode}");
+        }
+    }
+}
---
+++ System programming/WpfSystemApp2/ViewModel/MainViewModel.cs
@@ -0,0 +1,94 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Windows;
+using WpfSystemApp2.Services.Classes;
+namespace WpfSystemApp2.ViewModel;
+public class MainViewModel : ViewModelBase
+{
+    public int MinPrime { get; set; }
+    public int MaxPrime { get; set; }
+    public ObservableCollection<int> PrimeNumsList { get; set; } = new();
+    private static Thread? _primeThread;
+    public RelayCommand PrimeNumsStartContinueCommand
+    {
+        get => new(() =>
+        { 
+            if (PrimeNumbers.IsPaused)
+            {
+                PrimeNumbers.PauseEvent.Set();
+                PrimeNumbers.IsPaused = false;
+            }
+            else
+            {
+                _primeThread = new Thread(()=>PrimeNumbers.PrintPrimeNumbers(MinPrime, MaxPrime,PrimeNumsList) );
+                _primeThread.Start();
+            }
+        });
+    }
+    public RelayCommand PrimeNumsPause
+    {
+        get => new(() =>
+        {
+            PrimeNumbers.PauseEvent.Reset();
+            PrimeNumbers.IsPaused = true;
+            // If Max = 0 we can't continue process
+            if (MaxPrime == 0) PrimeNumbers.Cts.Cancel();
+            MessageBox.Show("Paused");
+        });
+    }
+    public RelayCommand PrimeNumsClear
+    {
+        get => new(() =>
+        {
+            PrimeNumsList.Clear();
+            PrimeNumbers.IsPaused = true;
+            PrimeNumbers.PauseEvent = new(true);
+            MinPrime = MaxPrime = 0;
+            MessageBox.Show("Cleaned");
+        });
+    }
+    //------------------------------------------------------------------------
+    public int FibonachyNumber { get; set; }
+    public ObservableCollection<int> FibonachyNumsList { get; set; } = new();
+    private static Thread? _fibonachyThread;
+    public RelayCommand FibonachyNumsStartContinueCommand
+    {
+        get => new(() =>
+        {
+            if (FibonachyNumbers.IsPaused)
+            {
+                FibonachyNumbers.PauseEvent.Set();
+                FibonachyNumbers.IsPaused = false;
+            }
+            else
+            {
+                _fibonachyThread = new Thread(() => FibonachyNumbers.PrintFibonachyNumbers(FibonachyNumber, FibonachyNumsList));
+                _fibonachyThread.Start();
+            }
+        });
+    }
+    public RelayCommand FibonachyNumsPause
+    {
+        get => new(() =>
+        {
+            FibonachyNumbers.PauseEvent.Reset();
+            FibonachyNumbers.IsPaused = true;
+            // If FibonachyNumber = 0 we can't continue process
+            if (FibonachyNumber == 0) FibonachyNumbers.Cts.Cancel();
+            MessageBox.Show("Paused");
+        });
+    }
+    public RelayCommand FibonachyNumsClear
+    {
+        get => new(() =>
+        {
+            FibonachyNumsList.Clear();
+            FibonachyNumbers.IsPaused = true;
+            FibonachyNumbers.PauseEvent = new(true);
+            FibonachyNumber = 0;
+            MessageBox.Show("Cleaned");
+        });
+    }
+}
